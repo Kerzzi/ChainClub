f0df0705db726b45b4a8eac394198b5c88f02c38
Kerzzi <kerzzi@outlook.com>
2017-12-20 19:32:50 +0800

    add validation to title and description

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,9 +17,11 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
- 
-    redirect_to groups_path
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end 
   end
   
   def update
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
 end
