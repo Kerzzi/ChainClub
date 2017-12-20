22a66c3d4e224609354813988c2a5de6b5d4aa95
Kerzzi <kerzzi@outlook.com>
2017-12-20 23:24:52 +0800

    add comments nested route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   resources :groups do
-    resources :posts
+    resources :posts do
+      resources comments
+    end
   end
 end
