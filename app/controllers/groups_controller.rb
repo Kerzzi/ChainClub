5db844aa843b337da3860e3fb064595d5cd6aa75
Kerzzi <kerzzi@outlook.com>
2017-12-20 20:58:51 +0800

    implement post create function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,6 +8,7 @@ class GroupsController < ApplicationController
   
   def show
     @group = Group.find(params[:id])
+    @posts = @group.posts
   end
 
   def edit
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,28 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end  
 end
