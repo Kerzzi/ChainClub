569ad475f4bbf200c3c0d91dabda6fae59b31204
Kerzzi <kerzzi@outlook.com>
2017-12-20 19:22:42 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,14 @@ class GroupsController < ApplicationController
  
     redirect_to groups_path
   end
+  
+  def update
+    @group = Group.find(params[:id])
+    
+    @group.update(group_params)
+    
+    redirect_to groups_path, notice:"更新成功！"
+  end
  
   private
  
