876d49e97166053088949e5fdc42b4698880e730
Kerzzi <kerzzi@outlook.com>
2017-12-30 15:18:34 +0800

    将默认时区改为北京时间

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module ChainClub
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = "zh-CN"
+    config.time_zone = "Beijing"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
