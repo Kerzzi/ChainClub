e6683c9e368366fee40f38406da8ef35ef82e2ea
Kerzzi <kerzzi@outlook.com>
2017-12-30 19:57:29 +0800

    拆开前后台的 CSS 和 JS

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require rails-ujs
+//= require bootstrap
+//= require select2
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require rails-ujs
 //= require bootstrap
-//= require select2
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
 //= require_tree .
