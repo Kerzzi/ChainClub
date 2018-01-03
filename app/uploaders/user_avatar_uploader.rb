aa5fca50a689c97901a779bb8f4a6645385c899c
Kerzzi <kerzzi@outlook.com>
2018-01-03 23:23:37 +0800

    头像功能新增Gravatar配置

--- app/uploaders/user_avatar_uploader.rb
+++ app/uploaders/user_avatar_uploader.rb
@@ -3,11 +3,7 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
-  version :thumb do
-    process resize_to_fit: [50, 50]
-  end
-
+  
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -18,6 +14,13 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  process resize_to_fit: [20, 20]
+  
+  version :thumb do
+    process resize_to_fill: [50, 50]
+  end
+
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
