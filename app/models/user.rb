99c6d42cd8d18768d6522bd666e674b383eca224
Kerzzi <kerzzi@outlook.com>
2018-01-04 20:54:57 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,4 +50,13 @@ class User < ApplicationRecord
   def is_member_of?(group)
     participated_groups.include?(group)
   end  
+  
+  def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+    
 end
