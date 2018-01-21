2ffe18d25809afc67f693365d37d5747ad39c8f2
Kerzzi <kerzzi@outlook.com>
2018-01-21 16:03:07 +0800

    完成项目搜索及搜索结果高亮的功能

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
   before_action :authenticate_user!, :only => [:new, :create]
+  before_action :validate_search_key, only: [:search]
 
   def index
     @projects = Project.all.paginate(:page => params[:page], :per_page => 10)
@@ -27,6 +28,27 @@ class ProjectsController < ApplicationController
       render :new
     end
   end
+
+   def search
+     if @query_string.present?
+       search_result = Project.ransack(@search_criteria).result(:distinct => true)
+       @projects = search_result.paginate(:page => params[:page], :per_page => 15 )
+     end
+   end
+
+
+   protected
+
+   # 取到params[:q]的内容并去掉非法的内容
+   def validate_search_key
+     @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+     @search_criteria = search_criteria(@query_string)
+   end
+
+
+   def search_criteria(query_string)
+     { :title_cont => query_string }
+   end
 
   private
 
--- app/models/project.rb
+++ app/models/project.rb
@@ -9,4 +9,13 @@ class Project < ApplicationRecord
   accepts_nested_attributes_for :project_grade
   
   mount_uploader :logo, ProjectLogoUploader
+  
+  #定义高亮显示功能的helper方法
+
+  def render_highlight_content(project,query_string)
+
+    excerpt_cont = excerpt(project.title, query_string, radius: 500)
+    highlight(excerpt_cont, query_string)
+  end
+  
 end
