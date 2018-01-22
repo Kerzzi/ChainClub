df81275299570c2f9b24661303321dc9c8300ffe
Kerzzi <kerzzi@outlook.com>
2018-01-23 00:26:53 +0800

    向首页增加活动内容

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     
-
+    @online_meetups = MeetupGroup.all.online_meetup.paginate(:page => params[:page], :per_page => 5)
+    @offline_meetups = MeetupGroup.all.offline_meetup.paginate(:page => params[:page], :per_page => 5)
     @internship_jobs = Job.published.recent.internship_job.paginate(:page => params[:page], :per_page => 4)
     @fulltime_jobs = Job.published.recent.fulltime_job.paginate(:page => params[:page], :per_page => 4)
     
