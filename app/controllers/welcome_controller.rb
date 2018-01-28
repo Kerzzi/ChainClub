94ea800e95dbeac591dc3942d941d4bca8f0332b
Kerzzi <kerzzi@outlook.com>
2018-01-28 23:33:32 +0800

    继续修改home页面

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-    @official_articles = OfficialArticle.where(:status => "public").order("created_at DESC").paginate(:page => params[:page], :per_page => 8)
-    @article_hots = OfficialArticle.where(:status => "public").sort_by{|official_article| -official_article.article_comments.count}
+    @official_articles = OfficialArticle.where(:status => "public").order("created_at DESC").paginate(:page => params[:page], :per_page => 7)
+    @article_hots = OfficialArticle.where(:status => "public").paginate(:page => params[:page], :per_page => 7).sort_by{|official_article| -official_article.article_comments.count}
+    @topics = Topic.published.paginate(:page => params[:page], :per_page => 8)
+    @groups = Group.published.recent.paginate(:page => params[:page], :per_page => 6)
+    @projects = Project.published.recent.paginate(:page => params[:page], :per_page => 4)
 
     @online_meetups = MeetupGroup.all.online_meetup.paginate(:page => params[:page], :per_page => 3)
     @offline_meetups = MeetupGroup.all.offline_meetup.paginate(:page => params[:page], :per_page => 3)
