29d7085a00ba1d2c9d7f0b7e4d7f70398431207a
Kerzzi <kerzzi@outlook.com>
2018-02-03 20:32:59 +0800

    fix bug

--- app/controllers/account/passwords_controller.rb
+++ app/controllers/account/passwords_controller.rb
@@ -1,10 +1,16 @@
 class Account::PasswordsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user
+  before_action :find_user
 
   def index
   end
 
-# 重置密码目前涉及setting controller，后期改掉
+  private
+
+  def find_user
+    @user = current_user
+  end
+
+  #重置密码目前涉及setting controller，后期改掉
 
 end
